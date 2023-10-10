958a2eed5d6625793cf5ba692702e565a36acc77
LJoel777 <szaknoel@gmail.com>
2023-10-10 09:08:27 +0200

    cors and missing images added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { driversRouter } from "./routes";
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use("/static", express.static("public/static"));
+app.use(cors());
 
 app.use("/api/drivers", driversRouter);
 
